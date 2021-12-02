f27874ab9ca0c615d4ce366ca534e759a469697a
Oliver Wang <oliver.wang@avalabs.org>
2021-12-02 10:17:22 -0800

    add images

--- scripts/splitData.js
+++ scripts/splitData.js
@@ -1,5 +1,10 @@
-const fs = require('fs')
-const path = require('path')
+import fs from 'fs';
+import fetch from 'node-fetch';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(
+    import.meta.url));
 const parseJSONFile = (path) => {
     try {
         const file = fs.readFileSync(path, 'utf8')
@@ -14,6 +19,16 @@ const parseJSONFile = (path) => {
 const file = parseJSONFile(path.join(__dirname, './ecosystem-backup.json'))
 
 
-file.projects.forEach((item) => {
+file.projects.forEach(async(item) => {
+    const resp = await fetch(item.imageURL)
+    const imgFilePath = path.join(__dirname, `../img/${item.slug}.${item.imageURL.split('.').slice(-1)}`)
+        // const fileStream = fs.createWriteStream(imgFilePath)
+        // await new Promise((resolve, reject) => {
+        //     resp.body.pipe(fileStream)
+        //     resp.body.on("error", reject)
+        //     fileStream.on("finish", resolve)
+        // })
+
+    item.imageURL = `/img/${item.slug}.${item.imageURL.split('.').slice(-1)}`
     fs.writeFileSync(`./projects/${item.slug}.json`, JSON.stringify(item, null, 2))
 })
