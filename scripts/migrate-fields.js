70fe87ff6e95f6a6ca159f564a7e7016a789eaf3
Oliver Wang <oliver.wang@avalabs.org>
2022-01-03 17:03:02 -0800

    update socials from bd

--- scripts/migrate-fields.js
+++ scripts/migrate-fields.js
@@ -1,38 +1,6 @@
-import fs from 'fs';
-import path, { dirname } from 'path';
+import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-const __dirname = dirname(
-  fileURLToPath(
-    // @ts-ignore
-    import.meta.url
-  )
-);
-const projectsPath = path.join(__dirname, '../projects');
-
-const parseJSONFile = (path) => {
-  try {
-    const file = fs.readFileSync(path, 'utf8');
-    return JSON.parse(file);
-  } catch (e) {
-    console.error(`error parsing ${path}`);
-    console.error(e);
-    process.exit(1);
-  }
-};
-const mapProjects = (mapper, dryRun = false) => {
-  const files = fs.readdirSync(projectsPath);
-  files
-    .map((fileName) => parseJSONFile(path.join(projectsPath, fileName)))
-    .map(mapper)
-    .forEach((item) => {
-      !dryRun
-        ? fs.writeFileSync(
-            `./projects/${item.slug}.json`,
-            JSON.stringify(item, null, 2)
-          )
-        : console.log(`item`, item);
-    });
-};
+import { mapProjects } from './mapProjects';
 
 const migrateFields = (item) => {
   const fieldMapper = {
